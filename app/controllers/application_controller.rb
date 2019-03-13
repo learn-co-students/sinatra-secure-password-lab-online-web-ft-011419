1392fa886ed2454b43ce2686c9058f76d9c2600c
Michael Schiumo <mschiumo18@gmail.com>
2019-03-13 19:53:25 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,9 +39,9 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
-    user = User.find_by(:username => params[:username])
+    @user = User.find_by(:username => params[:username])
 
-	  if user && user.authenticate(params[:password])
+	  if @user && @user.authenticate(params[:password])
 	    session[:user_id] = user.id
 	    redirect "/success"
 	  else
